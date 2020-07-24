cc7d344f0762609af0031f7204c75151945d7cf6
Cristhian Briones <cristhian.briones2016@uteq.edu.ec>
2020-07-23 20:05:41 -0500

    cm6

--- app/src/main/java/com/example/examenparc1/MainActivity.java
+++ app/src/main/java/com/example/examenparc1/MainActivity.java
@@ -11,7 +11,7 @@ import com.example.examenparc1.Model.Pais;
 import com.example.examenparc1.Model.adaptadorPais;
 import com.example.examenparc1.WebService.Asynchtask;
 import com.example.examenparc1.WebService.WebService;
-import com.mindorks.placeholderview.PlaceHolderView;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,7 +19,7 @@ import org.json.JSONException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
+/*
 public class MainActivity extends AppCompatActivity implements Asynchtask {
     RecyclerView recyclerview=null;
     @Override
@@ -57,5 +57,51 @@ public class MainActivity extends AppCompatActivity implements Asynchtask {
         //Envío la lista de productos a l
         adaptadorPais adapatorPa = new adaptadorPais(this, lstPais);
         recyclerview.setAdapter(adapatorPa);
+    }
+}*/
+
+
+
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+    ArrayList<String> listDatos;
+    RecyclerView recyclerview=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerview=(RecyclerView)findViewById(R.id.rcvListaHortalizas);
+
+        //añadir un Divider a los elementos de la lista->Diseño de la linea de separacion de los items
+        recyclerview.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
+        //Establecer el LayoutManager para definir la forma en la que se muestran los items en este caso en  forma de lista vertical
+        recyclerview.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        //Solicitud WebService
+        //https://my-json-server.typicode.com/SuannyTigselema/productos/producto
+        //https://mercado-a4435.firebaseio.com/producto.json
+        Map<String, String> datos = new HashMap<>();
+        //https://mercado-a4435.firebaseio.com/producto.json
+        WebService ws= new WebService("https://restcountries.eu/rest/v2/lang/es",datos,
+                MainActivity.this, (Asynchtask) MainActivity.this);
+
+        ws.execute("GET");
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        JSONArray JSONlistaProductos = new JSONArray(result);
+        ArrayList<Pais> lstProductos=new ArrayList<Pais>();
+
+        //Invoco al metodo de la clase productos que es para el parseo de datos-me devuelve un arraylist de producto
+        lstProductos = Pais.JsonObjectsBuild(JSONlistaProductos);
+
+        //HASTA AQUÍ SÍ RECIBE LOS 11 PRODUCTOS
+        //Envío la lista de productos a l
+        adaptadorPais adapatorHortalizas = new adaptadorPais(this, lstProductos);
+        recyclerview.setAdapter(adapatorHortalizas);
     }
 }
