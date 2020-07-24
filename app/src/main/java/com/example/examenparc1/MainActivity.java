55a6004f2aabbc4b2fb4a8d0cb022c70721b5fe0
Cristhian Briones <cristhian.briones2016@uteq.edu.ec>
2020-07-23 19:02:18 -0500

    cm4

--- app/src/main/java/com/example/examenparc1/MainActivity.java
+++ app/src/main/java/com/example/examenparc1/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.examenparc1.Model.Imagen;
+import com.mindorks.placeholderview.PlaceHolderView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PlaceHolderView mGalleryView = (PlaceHolderView)findViewById(R.id.galleryView);
+        mGalleryView
+                .addView(new Imagen(this.getApplicationContext(), mGalleryView, "https://restcountries.eu/rest/v2/alpha/ecu"));
+
     }
 }
